41d55bb0d137e73567b97d7f73e02568c111866f
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-11 17:11:00 +0100

    Removed fucking tabs.

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,106 +1,105 @@
 use core::marker::PhantomData;
 
-
 /// Reify a static pixel format to runtime.
 trait Pixel {
-	fn pixel_format() -> PixelFormat;
+  fn pixel_format() -> PixelFormat;
 }
 
 /// A `PixelFormat` gathers a `Type` along with a `Format`.
 struct PixelFormat {
-		encoding_type: Type
-	, format: Format
+    encoding_type: Type
+  , format: Format
 }
 
 /// Pixel type.
 enum Type {
-	  Integral
-	, Unsigned
-	, Floating
+    Integral
+  , Unsigned
+  , Floating
 }
 
 /// Format of a pixel.
 enum Format {
-		R(u8)
-	, RG(u8, u8)
-	, RGB(u8, u8, u8)
-	, RGBA(u8, u8, u8, u8)
-	, Depth(u8)
+    R(u8)
+  , RG(u8, u8)
+  , RGB(u8, u8, u8)
+  , RGBA(u8, u8, u8, u8)
+  , Depth(u8)
 }
 
 struct RGB8UI;
 
 impl Pixel for RGB8UI {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-				encoding_type: Type::Unsigned
-			, format: Format::RGB(8, 8, 8)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Unsigned
+      , format: Format::RGB(8, 8, 8)
+    }
+  }
 }
 
 struct RGBA8UI;
 
 impl Pixel for RGBA8UI {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-				encoding_type: Type::Unsigned
-			, format: Format::RGBA(8, 8, 8, 8)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Unsigned
+      , format: Format::RGBA(8, 8, 8, 8)
+    }
+  }
 }
 
 struct RGB8F;
 
 impl Pixel for RGB8F {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-				encoding_type: Type::Floating
-			, format: Format::RGB(8, 8, 8)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Floating
+      , format: Format::RGB(8, 8, 8)
+    }
+  }
 }
 
 struct RGBA8F;
 
 impl Pixel for RGBA8F {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-				encoding_type: Type::Floating
-			, format: Format::RGBA(8, 8, 8, 8)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Floating
+      , format: Format::RGBA(8, 8, 8, 8)
+    }
+  }
 }
 
 struct RGB32F;
 
 impl Pixel for RGB32F {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-				encoding_type: Type::Floating
-			, format: Format::RGB(32, 32, 32)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Floating
+      , format: Format::RGB(32, 32, 32)
+    }
+  }
 }
 
 struct RGBA32F;
 
 impl Pixel for RGBA32F {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-				encoding_type: Type::Floating
-			, format: Format::RGBA(32, 32, 32, 32)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Floating
+      , format: Format::RGBA(32, 32, 32, 32)
+    }
+  }
 }
 
 struct Depth32F;
 
 impl Pixel for Depth32F {
-	fn pixel_format() -> PixelFormat {
-		PixelFormat {
-			  encoding_type: Type::Floating
-			, format: Format::Depth(32)
-		}
-	}
+  fn pixel_format() -> PixelFormat {
+    PixelFormat {
+        encoding_type: Type::Floating
+      , format: Format::Depth(32)
+    }
+  }
 }
