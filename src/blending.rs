7c20c0d965b97f7c2d24f7d2724ae9b32197d412
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-17 10:58:30 +0100

    Added Factor and renamed mode into Equation.

--- src/blending.rs
+++ src/blending.rs
@@ -1,13 +1,16 @@
 //! That module exports blending-related types and functions.
-//! 
+//!
 //! Given two pixels *src* and *dst* – source and destination, we associate each pixel a blending
 //! factor – respectively, *srcK* and *dstK*. *src* is the pixel being computed, and *dst* is the
 //! pixel that is already stored in the framebuffer.
-//! 
-//! The pixels can be blended in several ways. See the documentation of `Mode` for further details.
-//! 
+//!
+//! The pixels can be blended in several ways. See the documentation of `Equation` for further
+//! details.
+//!
 //! The factors are encoded with `Factor`.
-pub enum Mode {
+
+/// Blending equation.
+pub enum Equation {
   /// `Additive` represents the following blending equation:
   ///
   /// > `blended = src * srcK + dst * dstK`
@@ -29,4 +32,28 @@ pub enum Mode {
   ///
   /// > `blended = max(src, dst)`
   Max
+}
+
+pub enum Factor {
+  /// 1 * color = factor
+  One,
+  /// 0 * color = 0
+  Zero,
+  /// src * color
+  SrcColor,
+  /// (1 - src) * color
+  NegativeSrcColor,
+  /// dst * color
+  DestColor,
+  /// (1 - dst) * color
+  NegativeDestColor,
+  /// srcA * color
+  SrcAlpha,
+  /// (1 - src) * color
+  NegativeSrcAlpha,
+  /// dstA * color
+  DstAlpha,
+  /// (1 - dstA) * color
+  NegativeDstAlpha,
+  SrcAlphaSaturate
 }
