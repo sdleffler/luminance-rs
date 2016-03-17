3790efe6005e38393c1c1d65d1ef66409567263e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-17 13:51:46 +0100

    Added Uniformable::value_size() to track the number of elements to send down to uniforms.

--- src/shader/uniform.rs
+++ src/shader/uniform.rs
@@ -29,121 +29,162 @@ pub struct Uniform<C, T> where C: HasUniform {
 }
 
 pub trait Uniformable {
+  /// Dimension of the uniform type.
   fn uniform_dim() -> Dim;
-
+  /// Type of the target value.
   fn value_type() -> Type;
+  /// Number of target values the uniform handles.
+  fn value_size() -> u32;
 }
 
 impl Uniformable for i32 {
   fn uniform_dim() -> Dim { Dim::DIM1 }
 
   fn value_type() -> Type { Type::Integral }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (i32, i32) {
   fn uniform_dim() -> Dim { Dim::DIM2 }
 
   fn value_type() -> Type { Type::Integral }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (i32, i32, i32) {
   fn uniform_dim() -> Dim { Dim::DIM3 }
 
   fn value_type() -> Type { Type::Integral }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (i32, i32, i32, i32) {
   fn uniform_dim() -> Dim { Dim::DIM4 }
 
   fn value_type() -> Type { Type::Integral }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for u32 {
   fn uniform_dim() -> Dim { Dim::DIM1 }
 
   fn value_type() -> Type { Type::Unsigned }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (u32, u32) {
   fn uniform_dim() -> Dim { Dim::DIM2 }
 
   fn value_type() -> Type { Type::Unsigned }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (u32, u32, u32) {
   fn uniform_dim() -> Dim { Dim::DIM3 }
 
   fn value_type() -> Type { Type::Unsigned }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (u32, u32, u32, u32) {
   fn uniform_dim() -> Dim { Dim::DIM4 }
 
   fn value_type() -> Type { Type::Unsigned }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for f32 {
   fn uniform_dim() -> Dim { Dim::DIM1 }
 
   fn value_type() -> Type { Type::Floating }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (f32, f32) {
   fn uniform_dim() -> Dim { Dim::DIM2 }
 
   fn value_type() -> Type { Type::Floating }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (f32, f32, f32) {
   fn uniform_dim() -> Dim { Dim::DIM3 }
 
   fn value_type() -> Type { Type::Floating }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (f32, f32, f32, f32) {
   fn uniform_dim() -> Dim { Dim::DIM4 }
 
   fn value_type() -> Type { Type::Floating }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for ((f32, f32), (f32, f32)) {
   fn uniform_dim() -> Dim { Dim::DIM22 }
 
   fn value_type() -> Type { Type::Floating}
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for ((f32, f32, f32), (f32, f32, f32), (f32, f32, f32)) {
   fn uniform_dim() -> Dim { Dim::DIM33 }
 
   fn value_type() -> Type { Type::Floating}
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for ((f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32)) {
   fn uniform_dim() -> Dim { Dim::DIM44 }
 
   fn value_type() -> Type { Type::Floating}
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for bool {
   fn uniform_dim() -> Dim { Dim::DIM1 }
 
   fn value_type() -> Type { Type::Boolean }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (bool, bool) {
   fn uniform_dim() -> Dim { Dim::DIM2 }
 
   fn value_type() -> Type { Type::Boolean }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (bool, bool, bool) {
   fn uniform_dim() -> Dim { Dim::DIM3 }
 
   fn value_type() -> Type { Type::Boolean }
+
+  fn value_size() -> u32 { 1 }
 }
 
 impl Uniformable for (bool, bool, bool, bool) {
   fn uniform_dim() -> Dim { Dim::DIM4 }
 
   fn value_type() -> Type { Type::Boolean }
+
+  fn value_size() -> u32 { 1 }
 }
