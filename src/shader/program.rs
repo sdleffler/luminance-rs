bae16e06a3cf65c1afbb16a1f3c3e05e5a229a4b
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-17 14:23:22 +0100

    HasProgram::new().

--- src/shader/program.rs
+++ src/shader/program.rs
@@ -1,6 +1,8 @@
 use shader::stage::HasStage;
+use shader::uniform;
 
-pub trait HasProgram: HasStage {
+pub trait HasProgram: HasStage + uniform::HasUniform {
   type Program;
 
+  fn new(tess: Option<(&Self::AStage, &Self::AStage)>, vertex: &Self::AStage, geometry: Option<&Self::AStage>, fragment: &Self::AStage) -> Self::Program;
 }
