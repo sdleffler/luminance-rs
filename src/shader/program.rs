88c7896f9b26c45597f9ff49a2d12f3118cf93fa
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-18 14:04:30 +0100

    Untabify.

--- src/shader/program.rs
+++ src/shader/program.rs
@@ -4,8 +4,8 @@ use shader::uniform::{HasUniform, UniformName};
 pub trait HasProgram: HasStage + HasUniform {
   type Program;
 
-	///
+  ///
   fn new(tess: Option<(&Self::AStage, &Self::AStage)>, vertex: &Self::AStage, geometry: Option<&Self::AStage>, fragment: &Self::AStage) -> Self::Program;
-	///
-	fn map_uniform(program: &Self::Program, name: UniformName) -> Option<Self::U>;
+  ///
+  fn map_uniform(program: &Self::Program, name: UniformName) -> Option<Self::U>;
 }
