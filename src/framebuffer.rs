6a6c41ab0ccdc2f3d9b5dc810b72d4695f94e6de
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-18 14:04:02 +0100

    Untabify.

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,11 +1,25 @@
-use texture;
+use core::marker::PhantomData;
+use pixel::Pixel;
+use texture::*;
 
 pub trait HasFramebuffer {
-	type AFramebuffer;
+  type AFramebuffer;
 
-	fn new<'a, D>(size: D::Size, mipmaps: u32) -> Result<Self::AFramebuffer, FramebufferError<'a>> where D: texture::Dimensionable;
+  fn new<'a, D>(size: D::Size, mipmaps: u32) -> Result<Self::AFramebuffer, FramebufferError<'a>> where D: Dimensionable;
 }
 
 pub enum FramebufferError<'a> {
-	Incomplete(&'a str)
+  Incomplete(&'a str)
+}
+
+pub struct Framebuffer<C, L, D, A, Color, Depth>
+    where C: HasTexture,
+          L: Layerable,
+          D: Dimensionable,
+          Color: Pixel,
+          Depth: Pixel {
+  color_tex: Option<Tex<C, L, D, Color>>,
+  depth_tex: Option<Tex<C, L, D, Depth>>,
+  _c: PhantomData<C>,
+  _a: PhantomData<A>,
 }
