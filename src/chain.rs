f350ee7a503e4b4cb5dd4b0a4afc68d4ced30f19
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-03-21 01:12:04 +0100

    Chain in vertex.

--- src/chain.rs
+++ src/chain.rs
@@ -1,24 +1,10 @@
-#![feature(type_macros)]
-
-extern crate core;
-
-use core::marker::PhantomData;
-
-struct Chain<A, B> {
-  _a: PhantomData<A>,
-  _b: PhantomData<B>
+pub struct Chain<A, B> {
+  pub left: A,
+  pub right: B
 }
 
 macro_rules! chain {
   ($t:ty) => {( $t )};
   
   ($a:ty, $($r:tt)*) => {( Chain<$a, chain![$($r)*]> )}
-}
-
-type Foo = tuple![bool];
-
-fn main() {
-  let foo: Foo = ();
-  
-  println!("{:?}", foo);
 }
