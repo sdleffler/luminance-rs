07f3c39727aa0ab126322b5c74f76f7910084540
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-21 01:34:16 +0100

    Framebuffer slots.

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,5 +1,7 @@
+use chain::Chain;
 use core::marker::PhantomData;
-use pixel::{ColorPixel, DepthPixel};
+use pixel::{ColorPixel, DepthPixel, PixelFormat};
+use std::vec::Vec;
 use texture::*;
 
 pub trait HasFramebuffer {
@@ -39,7 +41,29 @@ impl<C, L, D, A, Color, Depth> Framebuffer<C, L, D, A, Color, Depth>
 }
 */
 
+/// Slot type; used to create color and depth slots for framebuffers.
+pub struct Slot<T> {
+  _t: PhantomData<T>
+}
+
 /// A framebuffer has a color slot. A color slot can either be empty (the *unit* type is used,`()`)
 /// or several color formats. You can have up
-pub trait ColorSlot: ColorPixel {}
+pub trait ColorSlot {
+  fn color_slots() -> Vec<PixelFormat>;
+}
 
+impl ColorSlot for () {
+  fn color_slots() -> Vec<PixelFormat> { Vec::new() }
+}
+
+impl<P> ColorSlot for Slot<P> where P: ColorPixel {
+  fn color_slots() -> Vec<PixelFormat> { vec![P::pixel_format()] }
+}
+
+impl<A, B> ColorSlot for Chain<A,B> where A: ColorSlot, B: ColorSlot {
+  fn color_slots() -> Vec<PixelFormat> {
+    let mut a = A::color_slots();
+    a.extend(B::color_slots());
+    a
+  }
+}
