af1be6379aad53a7106210b218e74acbe9eda696
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-24 13:19:55 +0100

    render refactoring.

--- src/render.rs
+++ src/render.rs
@@ -1,8 +1,16 @@
 use blending;
 use framebuffer::{ColorSlot, DepthSlot, Framebuffer, HasFramebuffer};
+use rw::Writable;
 use shader::program::HasProgram;
 use tessellation::HasTessellation;
 use texture::HasTexture;
+
+pub trait HasFrameCommand: HasFramebuffer + HasProgram + HasTessellation + HasTexture + Sized {
+  fn run_frame_command<A, CS, DS>(cmd: &FrameCommand<Self, A, CS, DS>)
+    where A: Writable,
+          CS: ColorSlot,
+          DS: DepthSlot;
+}
 
 pub struct FrameCommand<C, A, CS, DS> 
     where C: HasFramebuffer + HasProgram + HasTessellation + HasTexture,
@@ -10,14 +18,6 @@ pub struct FrameCommand<C, A, CS, DS>
           DS: DepthSlot {
   pub framebuffer: Framebuffer<C, A, CS, DS>,
   pub shading_commands: Vec<ShadingCommand<C>>
-}
-
-impl<C, A, CS, DS> FrameCommand<C, A, CS, DS>
-    where C: HasFramebuffer + HasProgram + HasTessellation + HasTexture,
-          CS: ColorSlot,
-          DS: DepthSlot {
-  fn render(&self) {
-  }
 }
 
 pub struct ShadingCommand<C> where C: HasProgram + HasTessellation {
@@ -31,10 +31,4 @@ pub struct RenderCommand<C> where C: HasTessellation {
   pub depth_test: bool,
   pub update: Box<Fn()>,
   pub tessellation: C::Tessellation
-}
-
-impl<C> RenderCommand<C> where C: HasTessellation {
-  f)n render(&self) {
-    (self.update)();
-  }
 }
