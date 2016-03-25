e2e0b360553ea40e3dfc3c59a50d96bffd51b2c1
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-25 23:43:07 +0100

    Program creation might fail (because of shader stages linkage).

--- src/shader/program.rs
+++ src/shader/program.rs
@@ -5,7 +5,7 @@ pub trait HasProgram: HasStage + HasUniform {
   type Program;
 
   ///
-  fn new_program(tess: Option<(&Self::AStage, &Self::AStage)>, vertex: &Self::AStage, geometry: Option<&Self::AStage>, fragment: &Self::AStage) -> Self::Program;
+  fn new_program(tess: Option<(&Self::AStage, &Self::AStage)>, vertex: &Self::AStage, geometry: Option<&Self::AStage>, fragment: &Self::AStage) -> Result<Self::Program, ProgramError>;
   ///
   fn map_uniform(program: &Self::Program, name: UniformName) -> Option<Self::U>;
 }
@@ -15,7 +15,11 @@ pub struct Program<C> where C: HasProgram {
 }
 
 impl<C> Program<C> where C: HasProgram {
-	pub fn new(tess: Option<(&Stage<C, TessellationControlShader>, &Stage<C, TessellationEvaluationShader>)>, vertex: &Stage<C, VertexShader>, geometry: Option<&Stage<C, GeometryShader>>, fragment: &Stage<C, FragmentShader>) -> C::Program {
+	pub fn new(tess: Option<(&Stage<C, TessellationControlShader>, &Stage<C, TessellationEvaluationShader>)>, vertex: &Stage<C, VertexShader>, geometry: Option<&Stage<C, GeometryShader>>, fragment: &Stage<C, FragmentShader>) -> Result<C::Program, ProgramError> {
 		C::new_program(tess.map(|(tcs, tes)| (&tcs.repr, &tes.repr)), &vertex.repr, geometry.map(|g| &g.repr), &fragment.repr)
 	}
+}
+
+pub enum ProgramError {
+  LinkFailed(String)
 }
