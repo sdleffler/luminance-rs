5193bb5b1bc9df94777bde6ace906cddef7d85f7
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-26 00:16:19 +0100

    Drop instance for programs.

--- src/shader/program.rs
+++ src/shader/program.rs
@@ -7,12 +7,20 @@ pub trait HasProgram: HasStage + HasUniform {
   ///
   fn new_program(tess: Option<(&Self::AStage, &Self::AStage)>, vertex: &Self::AStage, geometry: Option<&Self::AStage>, fragment: &Self::AStage) -> Result<Self::Program, ProgramError>;
   ///
+  fn free_program(program: &mut Self::Program);
+  ///
   fn map_uniform(program: &Self::Program, name: UniformName) -> Option<Self::U>;
 }
 
 #[derive(Debug)]
 pub struct Program<C> where C: HasProgram {
 	pub repr: C::Program
+}
+
+impl<C> Drop for Program<C> where C: HasProgram {
+  fn drop(&mut self) {
+    C::free_program(&mut self.repr)
+  }
 }
 
 impl<C> Program<C> where C: HasProgram {
