afd7039d59c01be5e0743289364716f6c424c833
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-25 19:55:19 +0100

    Changed uniforms’ semantics.
    More boiler plate, but more efficient.

--- src/shader/uniform.rs
+++ src/shader/uniform.rs
@@ -7,43 +7,55 @@ pub trait HasUniform {
   /// Uniform representation.
   type U;
 
-  fn update<T>(uniform: &Self::U, dim: Dim, value_type: Type, value: &T);
-}
-
-/// Dimension of a `Uniform`.
-#[derive(Clone, Copy)]
-pub enum Dim {
-  DIM1,
-  DIM2,
-  DIM3,
-  DIM4,
-  DIM22,
-  DIM33,
-  DIM44
-}
-
-/// Type of a `Uniform`.
-#[derive(Clone, Copy)]
-pub enum Type {
-  Integral,
-  Unsigned,
-  Floating,
-  Boolean
+  // integral
+  fn update1_i32(uniform: &Self::U, x: i32);
+  fn update2_i32(uniform: &Self::U, xy: (i32, i32));
+  fn update3_i32(uniform: &Self::U, xyz: (i32, i32, i32));
+  fn update4_i32(uniform: &Self::U, xyzw: (i32, i32, i32, i32));
+  fn update1_vec_i32(uniform: &Self::U, x: &Vec<i32>);
+  fn update2_vec_i32(uniform: &Self::U, xy: &Vec<(i32, i32)>);
+  fn update3_vec_i32(uniform: &Self::U, xyz: &Vec<(i32, i32, i32)>);
+  fn update4_vec_i32(uniform: &Self::U, xyzw: &Vec<(i32, i32, i32, i32)>);
+  // unsigned
+  fn update1_u32(uniform: &Self::U, x: u32);
+  fn update2_u32(uniform: &Self::U, xy: (u32, u32));
+  fn update3_u32(uniform: &Self::U, xyz: (u32, u32, u32));
+  fn update4_u32(uniform: &Self::U, xyzw: (u32, u32, u32, u32));
+  fn update1_vec_u32(uniform: &Self::U, x: &Vec<u32>);
+  fn update2_vec_u32(uniform: &Self::U, xy: &Vec<(u32, u32)>);
+  fn update3_vec_u32(uniform: &Self::U, xyz: &Vec<(u32, u32, u32)>);
+  fn update4_vec_u32(uniform: &Self::U, xyzw: &Vec<(u32, u32, u32, u32)>);
+  // floating
+  fn update1_f32(uniform: &Self::U, x: f32);
+  fn update2_f32(uniform: &Self::U, xy: (f32, f32));
+  fn update3_f32(uniform: &Self::U, xyz: (f32, f32, f32));
+  fn update4_f32(uniform: &Self::U, xyzw: (f32, f32, f32, f32));
+  fn update1_vec_f32(uniform: &Self::U, x: &Vec<f32>);
+  fn update2_vec_f32(uniform: &Self::U, xy: &Vec<(f32, f32)>);
+  fn update3_vec_f32(uniform: &Self::U, xyz: &Vec<(f32, f32, f32)>);
+  fn update4_vec_f32(uniform: &Self::U, xyzw: &Vec<(f32, f32, f32, f32)>);
+  fn update22_f32(uniform: &Self::U, x: ((f32, f32), (f32, f32)));
+  fn update33_f32(uniform: &Self::U, x: ((f32, f32, f32), (f32, f32, f32), (f32, f32, f32)));
+  fn update44_f32(uniform: &Self::U, x: ((f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32)));
+  fn update22_vec_f32(uniform: &Self::U, x: &Vec<((f32, f32), (f32, f32))>);
+  fn update33_vec_f32(uniform: &Self::U, x: &Vec<((f32, f32, f32), (f32, f32, f32), (f32, f32, f32))>);
+  fn update44_vec_f32(uniform: &Self::U, x: &Vec<((f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32))>);
+  // boolean
+  fn update1_bool(uniform: &Self::U, x: bool);
+  fn update2_bool(uniform: &Self::U, xy: (bool, bool));
+  fn update3_bool(uniform: &Self::U, xyz: (bool, bool, bool));
+  fn update4_bool(uniform: &Self::U, xyzw: (bool, bool, bool, bool));
+  fn update1_vec_bool(uniform: &Self::U, x: &Vec<bool>);
+  fn update2_vec_bool(uniform: &Self::U, xy: &Vec<(bool, bool)>);
+  fn update3_vec_bool(uniform: &Self::U, xyz: &Vec<(bool, bool, bool)>);
+  fn update4_vec_bool(uniform: &Self::U, xyzw: &Vec<(bool, bool, bool, bool)>);
 }
 
 /// A shader uniform. `Uniform<C, T>` doesn’t hold any value. It’s more like a mapping between the
 /// host code and the shader the uniform was retrieved from.
 pub struct Uniform<C, T> where C: HasUniform, T: Uniformable {
   pub repr: C::U,
-  pub dim: Dim,
-  pub value_type: Type,
   _t: PhantomData<T>
-}
-
-impl<C, T> Uniform<C, T> where C: HasUniform, T: Uniformable {
-  pub fn update(&self, value: &T) {
-    C::update(&self.repr, self.dim, self.value_type, value)
-  }
 }
 
 /// Name of a `Uniform`.
@@ -53,171 +65,120 @@ pub enum UniformName<'a> {
 }
 
 /// Types that can behave as `Uniform`.
-pub trait Uniformable {
-  /// Dimension of the uniform type.
-  fn uniform_dim() -> Dim;
-  /// Type of the target value.
-  fn value_type() -> Type;
-  /// Number of target values the uniform handles.
-  fn value_size(_: &Self) -> usize;
+pub trait Uniformable: Sized {
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform;
 }
 
 impl Uniformable for i32 {
-  fn uniform_dim() -> Dim { Dim::DIM1 }
-
-  fn value_type() -> Type { Type::Integral }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update1_i32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (i32, i32) {
-  fn uniform_dim() -> Dim { Dim::DIM2 }
-
-  fn value_type() -> Type { Type::Integral }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update2_i32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (i32, i32, i32) {
-  fn uniform_dim() -> Dim { Dim::DIM3 }
-
-  fn value_type() -> Type { Type::Integral }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update3_i32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (i32, i32, i32, i32) {
-  fn uniform_dim() -> Dim { Dim::DIM4 }
-
-  fn value_type() -> Type { Type::Integral }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update4_i32(&u.repr, x)
+  }
 }
 
 impl Uniformable for u32 {
-  fn uniform_dim() -> Dim { Dim::DIM1 }
-
-  fn value_type() -> Type { Type::Unsigned }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update1_u32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (u32, u32) {
-  fn uniform_dim() -> Dim { Dim::DIM2 }
-
-  fn value_type() -> Type { Type::Unsigned }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update2_u32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (u32, u32, u32) {
-  fn uniform_dim() -> Dim { Dim::DIM3 }
-
-  fn value_type() -> Type { Type::Unsigned }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update3_u32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (u32, u32, u32, u32) {
-  fn uniform_dim() -> Dim { Dim::DIM4 }
-
-  fn value_type() -> Type { Type::Unsigned }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update4_u32(&u.repr, x)
+  }
 }
 
 impl Uniformable for f32 {
-  fn uniform_dim() -> Dim { Dim::DIM1 }
-
-  fn value_type() -> Type { Type::Floating }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update1_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (f32, f32) {
-  fn uniform_dim() -> Dim { Dim::DIM2 }
-
-  fn value_type() -> Type { Type::Floating }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update2_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (f32, f32, f32) {
-  fn uniform_dim() -> Dim { Dim::DIM3 }
-
-  fn value_type() -> Type { Type::Floating }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update3_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for (f32, f32, f32, f32) {
-  fn uniform_dim() -> Dim { Dim::DIM4 }
-
-  fn value_type() -> Type { Type::Floating }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update4_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for ((f32, f32), (f32, f32)) {
-  fn uniform_dim() -> Dim { Dim::DIM22 }
-
-  fn value_type() -> Type { Type::Floating}
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update22_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for ((f32, f32, f32), (f32, f32, f32), (f32, f32, f32)) {
-  fn uniform_dim() -> Dim { Dim::DIM33 }
-
-  fn value_type() -> Type { Type::Floating}
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update33_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for ((f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32), (f32, f32, f32, f32)) {
-  fn uniform_dim() -> Dim { Dim::DIM44 }
-
-  fn value_type() -> Type { Type::Floating}
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update44_f32(&u.repr, x)
+  }
 }
 
 impl Uniformable for bool {
-  fn uniform_dim() -> Dim { Dim::DIM1 }
-
-  fn value_type() -> Type { Type::Boolean }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update1_bool(&u.repr, x)
+  }
 }
 
 impl Uniformable for (bool, bool) {
-  fn uniform_dim() -> Dim { Dim::DIM2 }
-
-  fn value_type() -> Type { Type::Boolean }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update2_bool(&u.repr, x)
+  }
 }
 
 impl Uniformable for (bool, bool, bool) {
-  fn uniform_dim() -> Dim { Dim::DIM3 }
-
-  fn value_type() -> Type { Type::Boolean }
-
-  fn value_size(_: &Self) -> usize { 1 }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update3_bool(&u.repr, x)
+  }
 }
 
 impl Uniformable for (bool, bool, bool, bool) {
-  fn uniform_dim() -> Dim { Dim::DIM4 }
-
-  fn value_type() -> Type { Type::Boolean }
-
-  fn value_size(_: &Self) -> usize { 1 }
-}
-
-impl<T> Uniformable for Vec<T> where T: Uniformable {
-  fn uniform_dim() -> Dim { T::uniform_dim() }
-
-  fn value_type() -> Type { T::value_type() }
-
-  fn value_size(x: &Self) -> usize { x.len() }
+  fn update<C>(u: &Uniform<C, Self>, x: Self) where C: HasUniform {
+    C::update4_bool(&u.repr, x)
+  }
 }
