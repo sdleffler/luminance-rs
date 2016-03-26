52a497c6b222e8281484b39473b02d1046fe391c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-26 14:28:47 +0100

    Better semantics to update uniforms.

--- src/shader/program.rs
+++ src/shader/program.rs
@@ -10,26 +10,36 @@ pub trait HasProgram: HasStage + HasUniform {
   fn free_program(program: &mut Self::Program);
   ///
   fn map_uniform(program: &Self::Program, name: UniformName) -> Option<Self::U>;
+  ///
+  fn update_uniforms<F>(program: &Self::Program, f: F) where F: Fn();
 }
 
 #[derive(Debug)]
-pub struct Program<C> where C: HasProgram {
-	pub repr: C::Program
+pub struct Program<C, U> where C: HasProgram {
+	pub repr: C::Program,
+  pub uniforms: U
 }
 
-impl<C> Drop for Program<C> where C: HasProgram {
+impl<C, U> Drop for Program<C, U> where C: HasProgram {
   fn drop(&mut self) {
     C::free_program(&mut self.repr)
   }
 }
 
-impl<C> Program<C> where C: HasProgram {
-	pub fn new(tess: Option<(&Stage<C, TessellationControlShader>, &Stage<C, TessellationEvaluationShader>)>, vertex: &Stage<C, VertexShader>, geometry: Option<&Stage<C, GeometryShader>>, fragment: &Stage<C, FragmentShader>) -> Result<Program<C>, ProgramError> {
-		C::new_program(tess.map(|(tcs, tes)| (&tcs.repr, &tes.repr)), &vertex.repr, geometry.map(|g| &g.repr), &fragment.repr).map(|repr| Program { repr: repr })
+impl<C, U> Program<C, U> where C: HasProgram {
+	pub fn new<F>(tess: Option<(&Stage<C, TessellationControlShader>, &Stage<C, TessellationEvaluationShader>)>, vertex: &Stage<C, VertexShader>, geometry: Option<&Stage<C, GeometryShader>>, fragment: &Stage<C, FragmentShader>, f: F) -> Result<Program<C, U>, ProgramError> where F: Fn() -> U {
+		C::new_program(tess.map(|(tcs, tes)| (&tcs.repr, &tes.repr)), &vertex.repr, geometry.map(|g| &g.repr), &fragment.repr).map(|repr| Program {
+      repr: repr,
+      uniforms: f()
+    })
 	}
 
   pub fn uniform<T>(&self, name: &str) -> Option<Uniform<C, T>> where T: Uniformable {
     C::map_uniform(&self.repr, UniformName::StringName(String::from(name))).map(|u| Uniform::new(u))
+  }
+
+  pub fn update<F>(&self, f: F) where F: Fn() {
+    C::update_uniforms(&self.repr, f)
   }
 }
 
