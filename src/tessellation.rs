4ae720639942d6019c7412e2486e01ede9ca758e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-29 00:27:05 +0200

    Point and line size.

--- src/tessellation.rs
+++ src/tessellation.rs
@@ -45,9 +45,12 @@ pub trait HasTessellation {
   /// others in the order they appear. If you want to connect them in another way, you can index
   /// them with `Some(indices)`.
   fn new<T>(mode: Mode, vertices: &Vec<T>, indices: Option<&Vec<u32>>) -> Self::Tessellation where T: Vertex;
-  /// Render the tessellation. The `instances` parameter gives the number of instances to actually
-  /// draw.
-  fn render(tessellation: &Self::Tessellation, instances: u32);
+  /// Render the tessellation.
+  ///
+  /// The `size` argument gives, for points or lines, the size to use as thickness when rendering.
+  ///
+  /// The `instances` argument gives the number of instances to render.
+  fn render(tessellation: &Self::Tessellation, size: Option<f32>, instances: u32);
 }
 
 #[derive(Debug)]
