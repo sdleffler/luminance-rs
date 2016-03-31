3150862d79dc30afedb11427a0134eda8ff6cb72
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-03-31 14:34:55 +0200

    Render.

--- src/render.rs
+++ src/render.rs
@@ -1,9 +1,8 @@
 use blending;
 use framebuffer::{ColorSlot, DepthSlot, Framebuffer, HasFramebuffer};
-use rw::Writable;
 use shader::program::HasProgram;
 use tessellation::HasTessellation;
-use texture::HasTexture;
+use texture::{Dimensionable, HasTexture, Layerable};
 
 /*
 pub trait HasFrameCommand: HasFramebuffer + HasProgram + HasTessellation + HasTexture + Sized {
@@ -12,19 +11,23 @@ pub trait HasFrameCommand: HasFramebuffer + HasProgram + HasTessellation + HasTe
           CS: ColorSlot,
           DS: DepthSlot;
 }
+*/
 
-pub struct FrameCommand<C, A, CS, DS> 
-    where C: HasFramebuffer + HasProgram + HasTessellation + HasTexture,
-          CS: ColorSlot,
-          DS: DepthSlot {
-  pub framebuffer: Framebuffer<C, A, CS, DS>,
-  pub shading_commands: Vec<ShadingCommand<C>>
+pub struct FrameCommand<'a, C, L, D, CS, DS> 
+    where C: 'a + HasFramebuffer + HasProgram + HasTessellation + HasTexture,
+          L: Layerable,
+          D: Dimensionable,
+          D::Size: Copy,
+          CS: ColorSlot<C, L, D>,
+          DS: DepthSlot<C, L, D> {
+  pub framebuffer: Framebuffer<C, L, D, CS, DS>,
+  pub shading_commands: &'a [ShadingCommand<'a, C>]
 }
 
-pub struct ShadingCommand<C> where C: HasProgram + HasTessellation {
+pub struct ShadingCommand<'a, C> where C: 'a + HasProgram + HasTessellation {
   pub program: C::Program,
   pub update: Box<Fn()>,
-  pub render_commands: Vec<RenderCommand<C>>
+  pub render_commands: &'a [RenderCommand<C>]
 }
 
 pub struct RenderCommand<C> where C: HasTessellation {
@@ -33,4 +36,3 @@ pub struct RenderCommand<C> where C: HasTessellation {
   pub update: Box<Fn()>,
   pub tessellation: C::Tessellation
 }
-*/
