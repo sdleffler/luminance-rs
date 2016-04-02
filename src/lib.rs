fa75020eb16d741669f6993bd567aba10af7d21a
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-04-02 15:28:24 +0200

    UniformName.

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,10 @@ pub use pixel::{ColorPixel, DepthPixel, Format, Pixel, PixelFormat, is_color_pix
                 RGB8UI, RGBA8UI, RGB32F, RGBA32F, Depth32F};
 pub use render::run_frame_command;
 pub use rw::*;
+pub use shader::program::ProgramError;
 pub use shader::stage::{FragmentShader, GeometryShader, StageError, TessellationControlShader,
                         TessellationEvaluationShader, VertexShader};
+pub use shader::uniform::UniformName;
 pub use tessellation::Mode;
 pub use texture::{CubeFace, Cubemap, DepthComparison, Flat, Filter, Layered, Layering, Sampler,
                   Wrap};
