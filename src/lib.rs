541341f9397e5d811f1b31d58aefbe9c5907513c
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-04-02 15:21:26 +0200

    Re-exports.

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,19 @@ pub mod shader;
 pub mod tessellation;
 pub mod texture;
 pub mod vertex;
+
+// re-exports
+pub use blending::*;
+pub use buffer::BufferError;
+pub use chain::*;
+pub use framebuffer::{FramebufferError, default_framebuffer};
+pub use pixel::{ColorPixel, DepthPixel, Format, Pixel, PixelFormat, is_color_pixel, is_depth_pixel,
+                RGB8UI, RGBA8UI, RGB32F, RGBA32F, Depth32F};
+pub use render::run_frame_command;
+pub use rw::*;
+pub use shader::stage::{FragmentShader, GeometryShader, StageError, TessellationControlShader,
+                        TessellationEvaluationShader, VertexShader};
+pub use tessellation::Mode;
+pub use texture::{CubeFace, Cubemap, DepthComparison, Flat, Filter, Layered, Layering, Sampler,
+                  Wrap};
+pub use vertex::{VertexComponentFormat, VertexFormat};
