01b76652678ef1b4063f739eb49fd2283cdce433
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2016-06-11 13:03:40 +0200

    Fixed signature of UniformUpdate::update.

--- src/shader/uniform.rs
+++ src/shader/uniform.rs
@@ -103,7 +103,7 @@ impl<'a, C, T> From<Uniform<C, T>> for UniformUpdate<'a, T> where C: 'a + HasUni
 
 impl<'a, T> UniformUpdate<'a, T> where T: 'a {
   /// Update the underlying `Uniform`.
-  pub fn update(&'a self, x: T) {
+  pub fn update(&self, x: T) {
     (self.update_closure)(x)
   }
 
