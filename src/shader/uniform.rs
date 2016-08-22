76869073f1ad90d4fb439dd131029e1f6a042bd7
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-08-22 03:02:53 +0200

    Added dynamic reification of types, dimensions and sizes of uniforms.

--- src/shader/uniform.rs
+++ src/shader/uniform.rs
@@ -4,8 +4,8 @@
 
 use std::marker::PhantomData;
 use linear::*;
-use pixel::Pixel;
-use texture::{Dimensionable, Layerable, HasTexture, Texture};
+use pixel::{self, Pixel};
+use texture::{self, Dimensionable, Layerable, HasTexture, Texture};
 
 pub trait HasUniform: HasTexture {
   /// Uniform representation.
@@ -114,237 +114,499 @@ impl<'a, T> UniformUpdate<'a, T> where T: 'a {
   }
 }
 
+/// Type of a uniform.
+#[derive(Clone, Copy, Debug)]
+pub enum Type {
+  Integral,
+  Unsigned,
+  Floating,
+  Boolean,
+  ISampler,
+  USampler,
+  Sampler
+}
+
+/// Dimension of the uniform.
+#[derive(Clone, Copy, Debug)]
+pub enum Dim {
+  Dim1,
+  Dim2,
+  Dim3,
+  Dim4,
+  Dim22,
+  Dim33,
+  Dim44,
+  Cubemap
+}
+
+/// Layering of a uniform (flat or layered/array).
+
 /// Types that can behave as `Uniform`.
 pub trait Uniformable<C>: Sized where C: HasUniform {
+  /// Update the uniform with a new value.
   fn update(u: &Uniform<C, Self>, x: Self);
+  /// Retrieve the `Type` of the uniform.
+  fn reify_type() -> Type;
+  /// Retrieve the `Dim` of the uniform.
+  fn dim() -> Dim;
+  /// Retrieve the number of elements (if array), 1 otherwise.
+  fn size(&self) -> usize;
 }
 
 impl<C> Uniformable<C> for i32 where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [i32; 2] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [i32; 3] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [i32; 4] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<'a, C> Uniformable<C> for &'a [i32] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_slice_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[i32; 2]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_slice_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[i32; 3]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_slice_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[i32; 4]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_slice_i32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Integral }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<C> Uniformable<C> for u32 where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [u32; 2] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [u32; 3] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [u32; 4] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<'a, C> Uniformable<C> for &'a [u32] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_slice_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[u32; 2]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_slice_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[u32; 3]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_slice_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[u32; 4]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_slice_u32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Unsigned }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<C> Uniformable<C> for f32 where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [f32; 2] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [f32; 3] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [f32; 4] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<'a, C> Uniformable<C> for &'a [f32] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[f32; 2]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[f32; 3]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[f32; 4]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<C> Uniformable<C> for M22 where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update22_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim22 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for M33 where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update33_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim33 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for M44 where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update44_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim44 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<'a, C> Uniformable<C> for &'a [M22] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update22_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim22 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [M33] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update33_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim33 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [M44] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update44_slice_f32(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Floating }
+
+  fn dim() -> Dim { Dim::Dim44 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<C> Uniformable<C> for bool where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [bool; 2] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [bool; 3] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<C> Uniformable<C> for [bool; 4] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { 1 }
 }
 
 impl<'a, C> Uniformable<C> for &'a [bool] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update1_slice_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim1 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[bool; 2]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update2_slice_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim2 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[bool; 3]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update3_slice_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim3 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C> Uniformable<C> for &'a [[bool; 4]] where C: HasUniform {
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update4_slice_bool(&u.repr, x)
   }
+
+  fn reify_type() -> Type { Type::Boolean }
+
+  fn dim() -> Dim { Dim::Dim4 }
+
+  fn size(&self) -> usize { self.len() }
 }
 
 impl<'a, C, L, D, P> Uniformable<C> for &'a Texture<C, L, D, P>
@@ -355,4 +617,23 @@ impl<'a, C, L, D, P> Uniformable<C> for &'a Texture<C, L, D, P>
   fn update(u: &Uniform<C, Self>, x: Self) {
     C::update_textures(&u.repr, &[&x.repr]);
   }
+
+  fn reify_type() -> Type {
+    match P::pixel_format().encoding {
+      pixel::Type::Integral => Type::ISampler,
+      pixel::Type::Unsigned => Type::USampler,
+      pixel::Type::Floating => Type::Sampler
+    }
+  }
+
+  fn dim() -> Dim {
+    match D::dim() {
+      texture::Dim::Dim1 => Dim::Dim1,
+      texture::Dim::Dim2 => Dim::Dim2,
+      texture::Dim::Dim3 => Dim::Dim3,
+      texture::Dim::Cubemap => Dim::Cubemap,
+    }
+  }
+
+  fn size(&self) -> usize { 1 }
 }
