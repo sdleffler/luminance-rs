4c8d052da3ae0b83cfb1e2512096f02d0b479072
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-09-21 03:48:07 +0200

    ProgramProxy::uniform documentation.

--- luminance/src/shader/uniform_block.rs
+++ luminance/src/shader/uniform_block.rs
@@ -6,3 +6,7 @@ pub trait HasUniformBlock: HasBuffer {
 
   fn update_uniform_block(ub: &Self::UB, buffer: &Self::ABuffer, index: u32);
 }
+
+pub struct UniformBlock<C> where C: HasUniformBlock {
+  repr: C::UB
+}
