bd468912d3917cfaf58b58334d05d37f7ce277db
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-10-06 03:00:41 +0200

    Shader stage documentation.

--- luminance/src/shader/stage.rs
+++ luminance/src/shader/stage.rs
@@ -1,13 +1,19 @@
 use std::marker::PhantomData;
 
+/// Implement this trait to expose the concept of shader stages.
 pub trait HasStage {
+  /// Representation of a shader stage.
   type AStage;
 
+  /// Create a new shader stage with its type and its source code.
   fn new_shader(shader_type: Type, src: &str) -> Result<Self::AStage, StageError>;
+  /// Free a shader stage.
   fn free_shader(shader: &mut Self::AStage);
 }
 
+/// Class of types that are shader stage types.
 pub trait ShaderTypeable {
+  /// Reify a shader stage type to its runtime representation.
   fn shader_type() -> Type;
 }
 
@@ -21,35 +27,40 @@ pub enum Type {
   FragmentShader
 }
 
-#[derive(Debug)]
+/// Tessellation control shader. An optional stage.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct TessellationControlShader;
 
 impl ShaderTypeable for TessellationControlShader {
   fn shader_type() -> Type { Type::TessellationControlShader }
 }
 
-#[derive(Debug)]
+/// Tessellation evaluation shader. An optional stage
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct TessellationEvaluationShader;
 
 impl ShaderTypeable for TessellationEvaluationShader {
   fn shader_type() -> Type { Type::TessellationEvaluationShader }
 }
 
-#[derive(Debug)]
+/// Vertex shader. A mandatory stage
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct VertexShader;
 
 impl ShaderTypeable for VertexShader {
   fn shader_type() -> Type { Type::VertexShader }
 }
 
-#[derive(Debug)]
+/// Geometry shader. An optional stage
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct GeometryShader;
 
 impl ShaderTypeable for GeometryShader {
   fn shader_type() -> Type { Type::GeometryShader }
 }
 
-#[derive(Debug)]
+/// Fragment shader. A mandatory stage.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct FragmentShader;
 
 impl ShaderTypeable for FragmentShader {
@@ -79,6 +90,7 @@ impl<C, T> Stage<C, T> where C: HasStage, T: ShaderTypeable {
   }
 }
 
+/// Errors that shader stages can emit.
 #[derive(Clone, Debug)]
 pub enum StageError {
   /// Occurs when a shader fails to compile.
