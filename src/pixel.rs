f2e321c88ad8df66e6e442ffdd61c49221d65701
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-01-18 03:00:42 +0100

    Migrated pixel.rs. #31

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,6 +1,10 @@
+
 //! Pixel formats types and function manipulation.
 //!
 //! The `Pixel` trait is used to reify a pixel type at runtime via `PixelFormat`.
+
+use gl;
+use gl::types::*;
 
 /// Reify a static pixel format to runtime.
 pub trait Pixel {
@@ -162,3 +166,25 @@ impl Pixel for Depth32F {
 }
 
 impl DepthPixel for Depth32F {}
+
+// OpenGL format, internal sized-format and type.
+pub fn opengl_pixel_format(pf: PixelFormat) -> Option<(GLenum, GLenum, GLenum)> {
+  match (pf.format, pf.encoding) {
+    (Format::RGB(8, 8, 8), Type::Unsigned) => Some((gl::RGB_INTEGER, gl::RGB8UI, gl::UNSIGNED_BYTE)),
+    (Format::RGBA(8, 8, 8, 8), Type::Unsigned) => Some((gl::RGBA_INTEGER, gl::RGBA8UI, gl::UNSIGNED_BYTE)),
+    (Format::RGB(32, 32, 32), Type::Floating) => Some((gl::RGB, gl::RGB32F, gl::FLOAT)),
+    (Format::RGBA(32, 32, 32, 32), Type::Floating) => Some((gl::RGBA, gl::RGBA32F, gl::FLOAT)),
+    (Format::Depth(32), Type::Floating) => Some((gl::DEPTH_COMPONENT, gl::DEPTH_COMPONENT32F, gl::FLOAT)),
+    _ => panic!("unsupported pixel format")
+  }
+}
+
+// Return the number of components.
+pub fn pixel_components(pf: PixelFormat) -> usize {
+  match pf.format {
+    Format::RGB(_, _, _) => 3,
+    Format::RGBA(_, _, _, _) => 4,
+    Format::Depth(_) => 1,
+    _ => panic!("unsupported pixel format")
+  }
+}
