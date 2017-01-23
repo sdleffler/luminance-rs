5ab09dd38d55b9033932c70a6777ba02f2b81966
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-01-23 01:19:21 +0100

    TessVertices in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,7 @@ pub use pixel::{ColorPixel, DepthPixel, Format, Pixel, PixelFormat, RenderablePi
                 RGBA8UI, RGB32F, RGBA32F, Depth32F, is_color_pixel, is_depth_pixel};
 pub use shader::program::{Program, ProgramError, Sem, SemIndex, Uniform, UniformWarning, Uniformable};
 pub use shader::stage::{Stage, StageError};
-pub use tess::{Tess, Mode};
+pub use tess::{Tess, TessVertices, Mode};
 pub use texture::{CubeFace, Cubemap, DepthComparison, Dimensionable, Dim1, Dim2, Dim3, Flat, Filter,
                   Layerable, Layered, Layering, RawTexture, Sampler, Texture, Unit, Wrap};
 pub use vertex::{Vertex, VertexComponentFormat, VertexFormat};
