006ba57781f25ddf1d8688fc7fe02cfdb50b0181
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-03-19 00:06:12 +0100

    Fixing all examples for the vertex attribute binding.

--- luminance/examples/10-vertex-instancing/src/vs.glsl
+++ luminance/examples/10-vertex-instancing/src/vs.glsl
@@ -1,7 +1,7 @@
-layout (location = 0) in vec2 co;
-layout (location = 1) in vec3 color;
-layout (location = 2) in vec2 position;
-layout (location = 3) in float weight;
+in vec2 co;
+in vec3 color;
+in vec2 position;
+in float weight;
 
 out vec3 v_color;
 out float v_instance_bias;
