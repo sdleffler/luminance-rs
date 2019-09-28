7ef9fd37a281d10170a3227a97f83bb78ce76629
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-09-28 21:13:13 +0200

    Move DepthComparison into render_state.
    Relates to #173.

--- luminance/src/depth_test.rs
+++ luminance/src/depth_test.rs
@@ -1,5 +1,7 @@
 //! Depth test related features.
 
+use gl::types::*;
+
 /// Whether or not depth test should be enabled.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum DepthTest {
@@ -7,4 +9,41 @@ pub enum DepthTest {
   On,
   /// The depth test is disabled.
   Off,
+}
+
+/// Depth comparison to perform while depth test. `a` is the incoming fragment’s depth and b is the
+/// fragment’s depth that is already stored.
+#[derive(Clone, Copy, Debug)]
+pub enum DepthComparison {
+  /// Depth test never succeeds.
+  Never,
+  /// Depth test always succeeds.
+  Always,
+  /// Depth test succeeds if `a == b`.
+  Equal,
+  /// Depth test succeeds if `a != b`.
+  NotEqual,
+  /// Depth test succeeds if `a < b`.
+  Less,
+  /// Depth test succeeds if `a <= b`.
+  LessOrEqual,
+  /// Depth test succeeds if `a > b`.
+  Greater,
+  /// Depth test succeeds if `a >= b`.
+  GreaterOrEqual,
+}
+
+impl DepthComparison {
+  pub(crate) fn to_glenum(self) -> GLenum {
+    match self {
+      DepthComparison::Never => gl::NEVER,
+      DepthComparison::Always => gl::ALWAYS,
+      DepthComparison::Equal => gl::EQUAL,
+      DepthComparison::NotEqual => gl::NOTEQUAL,
+      DepthComparison::Less => gl::LESS,
+      DepthComparison::LessOrEqual => gl::LEQUAL,
+      DepthComparison::Greater => gl::GREATER,
+      DepthComparison::GreaterOrEqual => gl::GEQUAL,
+    }
+  }
 }
