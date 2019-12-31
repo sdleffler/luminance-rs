ac061bbc5120b353a38a75c37308a31a68f0531a
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-12-31 01:13:32 +0100

    Working on adding shader programs.

--- luminance/src/backend/shader.rs
+++ luminance/src/backend/shader.rs
@@ -48,10 +48,97 @@ impl fmt::Display for StageError {
   }
 }
 
+pub struct TessellationStages<'a, S> {
+  pub control: &'a S,
+  pub evaluation: &'a S,
+}
+
+/// Errors that a `Program` can generate.
+#[derive(Debug)]
+pub enum ProgramError {
+  /// A shader stage failed to compile or validate its state.
+  StageError(StageError),
+  /// Program link failed. You can inspect the reason by looking at the contained `String`.
+  LinkFailed(String),
+  /// Some uniform configuration is ill-formed. It can be a problem of inactive uniform, mismatch
+  /// type, etc. Check the `UniformWarning` type for more information.
+  UniformWarning(UniformWarning),
+  /// Some vertex attribute is ill-formed.
+  VertexAttribWarning(VertexAttribWarning),
+}
+
+impl fmt::Display for ProgramError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    match *self {
+      ProgramError::StageError(ref e) => write!(f, "shader program has stage error: {}", e),
+
+      ProgramError::LinkFailed(ref s) => write!(f, "shader program failed to link: {}", s),
+
+      ProgramError::UniformWarning(ref e) => {
+        write!(f, "shader program contains uniform warning(s): {}", e)
+      }
+      ProgramError::VertexAttribWarning(ref e) => write!(
+        f,
+        "shader program contains vertex attribute warning(s): {}",
+        e
+      ),
+    }
+  }
+}
+
+/// Warnings related to uniform issues.
+#[derive(Debug)]
+pub enum UniformWarning {
+  /// Inactive uniform (not in use / no participation to the final output in shaders).
+  Inactive(String),
+  /// Type mismatch between the static requested type (i.e. the `T` in [`Uniform<T>`] for instance)
+  /// and the type that got reflected from the backend in the shaders.
+  ///
+  /// The first `String` is the name of the uniform; the second one gives the type mismatch.
+  TypeMismatch(String, StageType),
+}
+
+impl fmt::Display for UniformWarning {
+  fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    match *self {
+      UniformWarning::Inactive(ref s) => write!(f, "inactive {} uniform", s),
+
+      UniformWarning::TypeMismatch(ref n, ref t) => {
+        write!(f, "type mismatch for uniform {}: {}", n, t)
+      }
+    }
+  }
+}
+
+/// Warnings related to vertex attributes issues.
+#[derive(Debug)]
+pub enum VertexAttribWarning {
+  /// Inactive vertex attribute (not read).
+  Inactive(String),
+}
+
+impl fmt::Display for VertexAttribWarning {
+  fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    match *self {
+      VertexAttribWarning::Inactive(ref s) => write!(f, "inactive {} vertex attribute", s),
+    }
+  }
+}
+
 pub unsafe trait Shader {
   type StageRepr;
+
+  type ProgramRepr;
 
   unsafe fn new_stage(&mut self, ty: StageType, src: &str) -> Result<Self::StageRepr, StageError>;
 
   unsafe fn destroy_stage(stage: &mut Self::StageRepr);
+
+  unsafe fn from_stages(
+    &mut self,
+    vertex: &Self::StageRepr,
+    tess: Option<TessellationStages<Self::StageRepr>>,
+    geometry: Option<&Self::StageRepr>,
+    fragment: &Self::StageRepr,
+  ) -> Result<Self::ProgramRepr, ProgramError>;
 }
