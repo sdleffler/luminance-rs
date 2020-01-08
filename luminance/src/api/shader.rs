21f17b6661ec3349d1d79de4fc7d6ee2614a70e8
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-01-08 01:52:55 +0100

    First complete draft of shader programs, uniform / uniform builders (backend).
    Relate to #262 #263.

--- luminance/src/api/shader.rs
+++ luminance/src/api/shader.rs
@@ -1,8 +1,13 @@
 //! Shader API.
 
-use crate::backend::shader::Shader;
-use crate::backend::shader_stage::{StageError, StageType};
+use std::marker::PhantomData;
+
+use crate::backend::shader::{
+  ProgramError, ProgramWarning, Shader, Uniform, UniformWarning, Uniformable,
+};
+use crate::backend::shader_stage::{StageError, StageType, TessellationStages};
 use crate::context::GraphicsContext;
+use crate::vertex::Semantics;
 
 pub struct Stage<S>
 where
@@ -35,5 +40,184 @@ where
 {
   fn drop(&mut self) {
     unsafe { S::destroy_stage(&mut self.repr) }
+  }
+}
+
+pub struct UniformBuilder<'a, S>
+where
+  S: Shader,
+{
+  repr: S::UniformBuilderRepr,
+  warnings: Vec<UniformWarning>,
+  _a: PhantomData<&'a mut ()>,
+}
+
+impl<'a, S> UniformBuilder<'a, S>
+where
+  S: Shader,
+{
+  pub fn ask<T, N>(&mut self, name: N) -> Result<Uniform<T>, UniformWarning>
+  where
+    N: AsRef<str>,
+    T: Uniformable<S>,
+  {
+    unsafe { S::ask_uniform(&mut self.repr, name.as_ref()) }
+  }
+
+  pub fn ask_or_unbound<T, N>(&mut self, name: N) -> Uniform<T>
+  where
+    N: AsRef<str>,
+    T: Uniformable<S>,
+  {
+    match self.ask(name) {
+      Ok(uniform) => uniform,
+      Err(err) => {
+        self.warnings.push(err);
+        unsafe { S::unbound(&mut self.repr) }
+      }
+    }
+  }
+}
+
+pub trait UniformInterface<E = ()>: Sized {
+  fn uniform_interface<'a, S>(
+    builder: &mut UniformBuilder<'a, S>,
+    env: &mut E,
+  ) -> Result<Self, UniformWarning>
+  where
+    S: Shader;
+}
+
+impl UniformInterface for () {
+  fn uniform_interface<'a, S>(
+    _: &mut UniformBuilder<'a, S>,
+    _: &mut (),
+  ) -> Result<Self, UniformWarning>
+  where
+    S: Shader,
+  {
+    Ok(())
+  }
+}
+
+/// A built program with potential warnings.
+///
+/// The sole purpose of this type is to be destructured when a program is built.
+pub struct BuiltProgram<S, Sem, Out, Uni>
+where
+  S: Shader,
+{
+  /// Built program.
+  pub program: Program<S, Sem, Out, Uni>,
+  /// Potential warnings.
+  pub warnings: Vec<ProgramError>,
+}
+
+impl<S, Sem, Out, Uni> BuiltProgram<S, Sem, Out, Uni>
+where
+  S: Shader,
+{
+  /// Get the program and ignore the warnings.
+  pub fn ignore_warnings(self) -> Program<S, Sem, Out, Uni> {
+    self.program
+  }
+}
+
+/// A [`Program`] uniform adaptation that has failed.
+pub struct AdaptationFailure<S, Sem, Out, Uni>
+where
+  S: Shader,
+{
+  /// Program used before trying to adapt.
+  pub program: Program<S, Sem, Out, Uni>,
+  /// Program error that prevented to adapt.
+  pub error: ProgramError,
+}
+
+impl<S, Sem, Out, Uni> AdaptationFailure<S, Sem, Out, Uni>
+where
+  S: Shader,
+{
+  /// Get the program and ignore the error.
+  pub fn ignore_error(self) -> Program<S, Sem, Out, Uni> {
+    self.program
+  }
+}
+
+pub struct Program<S, Sem, Out, Uni>
+where
+  S: Shader,
+{
+  repr: S::ProgramRepr,
+  uni: Uni,
+  _sem: PhantomData<*const Sem>,
+  _out: PhantomData<*const Out>,
+}
+
+impl<S, Sem, Out, Uni> Drop for Program<S, Sem, Out, Uni>
+where
+  S: Shader,
+{
+  fn drop(&mut self) {
+    unsafe { S::destroy_program(&mut self.repr) }
+  }
+}
+
+impl<S, Sem, Out, Uni> Program<S, Sem, Out, Uni>
+where
+  S: Shader,
+  Sem: Semantics,
+{
+  pub fn from_stages_env<C, T, G, E>(
+    ctx: &mut C,
+    vertex: &Stage<S>,
+    tess: T,
+    geometry: G,
+    fragment: &Stage<S>,
+    env: &mut E,
+  ) -> Result<BuiltProgram<S, Sem, Out, Uni>, ProgramError>
+  where
+    C: GraphicsContext<Backend = S>,
+    Uni: UniformInterface<E>,
+    T: for<'a> Into<Option<TessellationStages<'a, Stage<S>>>>,
+    G: for<'a> Into<Option<&'a Stage<S>>>,
+  {
+    let tess = tess.into();
+    let geometry = geometry.into();
+
+    unsafe {
+      let mut repr = ctx.backend().new_program(
+        &vertex.repr,
+        tess.map(|stages| TessellationStages {
+          control: &stages.control.repr,
+          evaluation: &stages.evaluation.repr,
+        }),
+        geometry.map(|stage| &stage.repr),
+        &fragment.repr,
+      )?;
+
+      let warnings = S::apply_semantics::<Sem>(&mut repr)?
+        .into_iter()
+        .map(|w| ProgramError::Warning(ProgramWarning::VertexAttrib(w)))
+        .collect();
+
+      let mut uniform_builder: UniformBuilder<S> =
+        S::new_uniform_builder(&mut repr).map(|repr| UniformBuilder {
+          repr,
+          warnings: Vec::new(),
+          _a: PhantomData,
+        })?;
+      let uni = Uni::uniform_interface(&mut uniform_builder, env)
+        .map_err(|w| ProgramError::Warning(ProgramWarning::Uniform(w)))?;
+
+      let program = Program {
+        repr,
+        uni,
+        _sem: PhantomData,
+        _out: PhantomData,
+      };
+
+      Ok(BuiltProgram { program, warnings })
+    }
   }
 }
--- luminance/src/backend/shader.rs
+++ luminance/src/backend/shader.rs
@@ -4,6 +4,8 @@ use std::fmt;
 use std::marker::PhantomData;
 
 use crate::backend::shader_stage::{StageError, StageType, TessellationStages};
+use crate::vertex::Semantics;
+
 /// Errors that a `Program` can generate.
 #[derive(Debug)]
 pub enum ProgramError {
@@ -11,11 +13,7 @@ pub enum ProgramError {
   StageError(StageError),
   /// Program link failed. You can inspect the reason by looking at the contained `String`.
   LinkFailed(String),
-  /// Some uniform configuration is ill-formed. It can be a problem of inactive uniform, mismatch
-  /// type, etc. Check the `UniformWarning` type for more information.
-  UniformWarning(UniformWarning),
-  /// Some vertex attribute is ill-formed.
-  VertexAttribWarning(VertexAttribWarning),
+  Warning(ProgramWarning),
 }
 
 impl fmt::Display for ProgramError {
@@ -25,14 +23,7 @@ impl fmt::Display for ProgramError {
 
       ProgramError::LinkFailed(ref s) => write!(f, "shader program failed to link: {}", s),
 
-      ProgramError::UniformWarning(ref e) => {
-        write!(f, "shader program contains uniform warning(s): {}", e)
-      }
-      ProgramError::VertexAttribWarning(ref e) => write!(
-        f,
-        "shader program contains vertex attribute warning(s): {}",
-        e
-      ),
+      ProgramError::Warning(ref e) => write!(f, "shader program warning: {}", e),
     }
   }
 }
@@ -61,6 +52,25 @@ impl fmt::Display for UniformWarning {
   }
 }
 
+/// Program warnings, not necessarily considered blocking errors.
+#[derive(Debug)]
+pub enum ProgramWarning {
+  /// Some uniform configuration is ill-formed. It can be a problem of inactive uniform, mismatch
+  /// type, etc. Check the `UniformWarning` type for more information.
+  Uniform(UniformWarning),
+  /// Some vertex attribute is ill-formed.
+  VertexAttrib(VertexAttribWarning),
+}
+
+impl fmt::Display for ProgramWarning {
+  fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    match *self {
+      ProgramWarning::Uniform(ref e) => write!(f, "uniform warning: {}", e),
+      ProgramWarning::VertexAttrib(ref e) => write!(f, "vertex attribute warning: {}", e),
+    }
+  }
+}
+
 /// Warnings related to vertex attributes issues.
 #[derive(Debug)]
 pub enum VertexAttribWarning {
@@ -76,9 +86,138 @@ impl fmt::Display for VertexAttribWarning {
   }
 }
 
-pub struct Uniform<T> {
+pub struct Uniform<T>
+where
+  T: ?Sized,
+{
   index: i32,
   _t: PhantomData<*const T>,
+}
+
+/// Type of a uniform.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum UniformType {
+  // scalars
+  /// 32-bit signed integer.
+  Int,
+  /// 32-bit unsigned integer.
+  UInt,
+  /// 32-bit floating-point number.
+  Float,
+  /// Boolean.
+  Bool,
+
+  // vectors
+  /// 2D signed integral vector.
+  IVec2,
+  /// 3D signed integral vector.
+  IVec3,
+  /// 4D signed integral vector.
+  IVec4,
+  /// 2D unsigned integral vector.
+  UIVec2,
+  /// 3D unsigned integral vector.
+  UIVec3,
+  /// 4D unsigned integral vector.
+  UIVec4,
+  /// 2D floating-point vector.
+  Vec2,
+  /// 3D floating-point vector.
+  Vec3,
+  /// 4D floating-point vector.
+  Vec4,
+  /// 2D boolean vector.
+  BVec2,
+  /// 3D boolean vector.
+  BVec3,
+  /// 4D boolean vector.
+  BVec4,
+
+  // matrices
+  /// 2×2 floating-point matrix.
+  M22,
+  /// 3×3 floating-point matrix.
+  M33,
+  /// 4×4 floating-point matrix.
+  M44,
+
+  // textures
+  /// Signed integral 1D texture sampler.
+  ISampler1D,
+  /// Signed integral 2D texture sampler.
+  ISampler2D,
+  /// Signed integral 3D texture sampler.
+  ISampler3D,
+  /// Unsigned integral 1D texture sampler.
+  UISampler1D,
+  /// Unsigned integral 2D texture sampler.
+  UISampler2D,
+  /// Unsigned integral 3D texture sampler.
+  UISampler3D,
+  /// Floating-point 1D texture sampler.
+  Sampler1D,
+  /// Floating-point 2D texture sampler.
+  Sampler2D,
+  /// Floating-point 3D texture sampler.
+  Sampler3D,
+  /// Signed cubemap sampler.
+  ICubemap,
+  /// Unsigned cubemap sampler.
+  UICubemap,
+  /// Floating-point cubemap sampler.
+  Cubemap,
+
+  // buffer
+  /// Buffer binding; used for UBOs.
+  BufferBinding,
+}
+
+impl fmt::Display for UniformType {
+  fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    match *self {
+      UniformType::Int => f.write_str("int"),
+      UniformType::UInt => f.write_str("uint"),
+      UniformType::Float => f.write_str("float"),
+      UniformType::Bool => f.write_str("bool"),
+      UniformType::IVec2 => f.write_str("ivec2"),
+      UniformType::IVec3 => f.write_str("ivec3"),
+      UniformType::IVec4 => f.write_str("ivec4"),
+      UniformType::UIVec2 => f.write_str("uvec2"),
+      UniformType::UIVec3 => f.write_str("uvec3"),
+      UniformType::UIVec4 => f.write_str("uvec4"),
+      UniformType::Vec2 => f.write_str("vec2"),
+      UniformType::Vec3 => f.write_str("vec3"),
+      UniformType::Vec4 => f.write_str("vec4"),
+      UniformType::BVec2 => f.write_str("bvec2"),
+      UniformType::BVec3 => f.write_str("bvec3"),
+      UniformType::BVec4 => f.write_str("bvec4"),
+      UniformType::M22 => f.write_str("mat2"),
+      UniformType::M33 => f.write_str("mat3"),
+      UniformType::M44 => f.write_str("mat4"),
+      UniformType::ISampler1D => f.write_str("isampler1D"),
+      UniformType::ISampler2D => f.write_str("isampler2D"),
+      UniformType::ISampler3D => f.write_str("isampler3D"),
+      UniformType::UISampler1D => f.write_str("uSampler1D"),
+      UniformType::UISampler2D => f.write_str("uSampler2D"),
+      UniformType::UISampler3D => f.write_str("uSampler3D"),
+      UniformType::Sampler1D => f.write_str("sampler1D"),
+      UniformType::Sampler2D => f.write_str("sampler2D"),
+      UniformType::Sampler3D => f.write_str("sampler3D"),
+      UniformType::ICubemap => f.write_str("isamplerCube"),
+      UniformType::UICubemap => f.write_str("usamplerCube"),
+      UniformType::Cubemap => f.write_str("samplerCube"),
+      UniformType::BufferBinding => f.write_str("buffer binding"),
+    }
+  }
+}
+
+pub unsafe trait Uniformable<S>
+where
+  S: ?Sized + Shader,
+{
+  unsafe fn update(self, program: &mut S::ProgramRepr, uniform: &mut Uniform<Self>);
+
+  unsafe fn ty() -> UniformType;
 }
 
 pub unsafe trait Shader {
@@ -86,15 +225,40 @@ pub unsafe trait Shader {
 
   type ProgramRepr;
 
+  type UniformBuilderRepr;
+
   unsafe fn new_stage(&mut self, ty: StageType, src: &str) -> Result<Self::StageRepr, StageError>;
 
   unsafe fn destroy_stage(stage: &mut Self::StageRepr);
 
-  unsafe fn from_stages(
+  unsafe fn new_program(
     &mut self,
     vertex: &Self::StageRepr,
     tess: Option<TessellationStages<Self::StageRepr>>,
     geometry: Option<&Self::StageRepr>,
     fragment: &Self::StageRepr,
   ) -> Result<Self::ProgramRepr, ProgramError>;
+
+  unsafe fn destroy_program(program: &mut Self::ProgramRepr);
+
+  unsafe fn apply_semantics<Sem>(
+    program: &mut Self::ProgramRepr,
+  ) -> Result<Vec<VertexAttribWarning>, ProgramError>
+  where
+    Sem: Semantics;
+
+  unsafe fn new_uniform_builder(
+    program: &mut Self::ProgramRepr,
+  ) -> Result<Self::UniformBuilderRepr, ProgramError>;
+
+  unsafe fn ask_uniform<T>(
+    uniform_builder: &mut Self::UniformBuilderRepr,
+    name: &str,
+  ) -> Result<Uniform<T>, UniformWarning>
+  where
+    T: Uniformable<Self>;
+
+  unsafe fn unbound<T>(uniform_builder: &mut Self::UniformBuilderRepr) -> Uniform<T>
+  where
+    T: Uniformable<Self>;
 }
