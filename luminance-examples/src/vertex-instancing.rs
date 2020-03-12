f401dd9c2cdfc669cd5175a304d73c6a74dc0c40
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-03-12 17:12:20 +0100

    Migrate the vertex-instancing example.

--- luminance-examples/src/vertex-instancing.rs
+++ luminance-examples/src/vertex-instancing.rs
@@ -9,12 +9,14 @@ mod common;
 use crate::common::{
   Instance, Semantics, Vertex, VertexColor, VertexInstancePosition, VertexPosition, VertexWeight,
 };
-use luminance::context::GraphicsContext;
+use glfw::{Action, Context as _, Key, WindowEvent};
+use luminance::context::GraphicsContext as _;
 use luminance::pipeline::PipelineState;
 use luminance::render_state::RenderState;
-use luminance::shader::program::Program;
+use luminance::shader::Program;
 use luminance::tess::{Mode, TessBuilder};
-use luminance_glfw::{Action, GlfwSurface, Key, Surface, WindowDim, WindowEvent, WindowOpt};
+use luminance_glfw::GlfwSurface;
+use luminance_windowing::{WindowDim, WindowOpt};
 use std::time::Instant;
 
 const VS: &'static str = include_str!("instancing-vs.glsl");
@@ -61,23 +63,26 @@ const INSTANCES: [Instance; 5] = [
 ];
 
 fn main() {
-  let mut surface = GlfwSurface::new(
-    WindowDim::Windowed(960, 540),
+  let mut surface = GlfwSurface::new_gl33(
+    WindowDim::Windowed {
+      width: 960,
+      height: 540,
+    },
     "Hello, world!",
     WindowOpt::default(),
   )
   .expect("GLFW surface creation");
 
   // notice that we don’t set a uniform interface here: we’re going to look it up on the fly
-  let program = Program::<Semantics, (), ()>::from_strings(None, VS, None, FS)
+  let mut program = Program::<_, Semantics, (), ()>::from_strings(&mut surface, VS, None, None, FS)
     .expect("program creation")
     .ignore_warnings();
 
   let triangle = TessBuilder::new(&mut surface)
-    .add_vertices(TRI_VERTICES)
-    .add_instances(INSTANCES)
-    .set_mode(Mode::Triangle)
-    .build()
+    .and_then(|b| b.add_vertices(TRI_VERTICES))
+    .and_then(|b| b.add_instances(INSTANCES))
+    .and_then(|b| b.set_mode(Mode::Triangle))
+    .and_then(|b| b.build())
     .unwrap();
 
   let mut back_buffer = surface.back_buffer().unwrap();
@@ -88,7 +93,8 @@ fn main() {
   let mut resize = false;
 
   'app: loop {
-    for event in surface.poll_events() {
+    surface.window.glfw.poll_events();
+    for (_, event) in surface.events_rx.try_iter() {
       match event {
         WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => break 'app,
 
@@ -133,15 +139,13 @@ fn main() {
     let t64 = elapsed.as_secs() as f64 + (elapsed.subsec_millis() as f64 * 1e-3);
     let t = t64 as f32;
 
-    surface.pipeline_builder().pipeline(
+    let render = surface.pipeline_gate().pipeline(
       &back_buffer,
       &PipelineState::default(),
       |_, mut shd_gate| {
-        shd_gate.shade(&program, |iface, mut rdr_gate| {
-          let query = iface.query();
-
-          if let Ok(time_u) = query.ask("t") {
-            time_u.update(t);
+        shd_gate.shade(&mut program, |mut iface, _, mut rdr_gate| {
+          if let Ok(ref time_u) = iface.query().unwrap().ask("t") {
+            iface.set(time_u, t);
           }
 
           rdr_gate.render(&RenderState::default(), |mut tess_gate| {
@@ -151,6 +155,10 @@ fn main() {
       },
     );
 
-    surface.swap_buffers();
+    if render.is_ok() {
+      surface.window.swap_buffers();
+    } else {
+      break 'app;
+    }
   }
 }
