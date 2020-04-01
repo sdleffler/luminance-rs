85d11a8adcaa35cd0cb51c9c440c04a786d22812
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-04-02 00:20:26 +0200

    Document render gates.

--- luminance/src/render_gate.rs
+++ luminance/src/render_gate.rs
@@ -1,8 +1,23 @@
+//! Render gates.
+//!
+//! A render gate is a _pipeline node_ that allows to share [`RenderState`] for deeper nodes,
+//! which are used to render [`Tess`].
+//!
+//! [`Tess`]: crate::tess::Tess
+
 use crate::backend::render_gate::RenderGate as RenderGateBackend;
 use crate::context::GraphicsContext;
 use crate::render_state::RenderState;
 use crate::tess_gate::TessGate;
 
+/// A render gate.
+///
+/// # Parametricity
+///
+/// - `C` is the backend type. It must implement [`GraphicsContext`] and `C::Backend` must
+///   implement [`backend::RenderGate`].
+///
+/// [`backend::RenderGate`]: crate::backend::render_gate::RenderGate
 pub struct RenderGate<'a, C>
 where
   C: ?Sized,
@@ -15,6 +30,7 @@ where
   C: ?Sized + GraphicsContext,
   C::Backend: RenderGateBackend,
 {
+  /// Enter a [`RenderGate`] and go deeper in the pipeline.
   pub fn render<'b, F>(&'b mut self, rdr_st: &RenderState, f: F)
   where
     F: FnOnce(TessGate<'b, C>),
