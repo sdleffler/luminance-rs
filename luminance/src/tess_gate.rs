d86878a855cc5bf3577481356cf726ff08a21dad
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-04-05 02:53:57 +0200

    Re-document tessellation gates.

--- luminance/src/tess_gate.rs
+++ luminance/src/tess_gate.rs
@@ -1,7 +1,14 @@
+//! Tessellation gates.
+//!
+//! A tessellation gate is a _pipeline node_ that allows to share [`Tess`] for deeper nodes.
+//!
+//! [`Tess`]: crate::tess::Tess
+
 use crate::backend::tess_gate::TessGate as TessGateBackend;
 use crate::context::GraphicsContext;
 use crate::tess::TessView;
 
+/// Tessellation gates.
 pub struct TessGate<'a, C>
 where
   C: ?Sized,
@@ -14,6 +21,7 @@ where
   C: ?Sized + GraphicsContext,
   C::Backend: TessGateBackend,
 {
+  /// Enter the [`TessGate`] by sharing a [`TessView`].
   pub fn render<'b, T>(&'b mut self, tess_view: T)
   where
     T: Into<TessView<'b, C::Backend>>,
