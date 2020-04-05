45e307ac90b7830106434cf4a12463a1054ffa11
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-04-05 02:51:46 +0200

    Re-document shading gates.

--- luminance/src/shading_gate.rs
+++ luminance/src/shading_gate.rs
@@ -1,9 +1,24 @@
+//! Shading gates.
+//!
+//! A shading gate is a _pipeline node_ that allows to share shader [`Program`] for deeper nodes.
+//!
+//! [`Program`]: crate::shader::Program
+
 use crate::backend::shading_gate::ShadingGate as ShadingGateBackend;
 use crate::context::GraphicsContext;
 use crate::render_gate::RenderGate;
 use crate::shader::{Program, ProgramInterface, UniformInterface};
 use crate::vertex::Semantics;
 
+/// A shading gate.
+///
+/// This is obtained after entering a [`PipelineGate`].
+///
+/// # Parametricity
+///
+/// - `C` is the backend type.
+///
+/// [`PipelineGate`]: crate::pipeline::PipelineGate
 pub struct ShadingGate<'a, C>
 where
   C: ?Sized + GraphicsContext,
@@ -17,6 +32,13 @@ where
   C: ?Sized + GraphicsContext,
   C::Backend: ShadingGateBackend,
 {
+  /// Enter a [`ShadingGate`] by using a shader [`Program`].
+  ///
+  /// The argument closure is given two arguments:
+  ///
+  /// - A [`ProgramInterface`], that allows to pass values (via [`ProgramInterface::set`]) to the
+  ///   in-use shader [`Program`] and/or perform dynamic lookup of uniforms.
+  /// - A [`RenderGate`], allowing to create deeper nodes in the graphics pipeline.
   pub fn shade<Sem, Out, Uni, F>(&mut self, program: &mut Program<C::Backend, Sem, Out, Uni>, f: F)
   where
     Sem: Semantics,
