125ea6e14fd752efa280c5378c3514399f8d14b6
Deokhwan Kim <dkim@users.noreply.github.com>
2020-07-25 20:49:44 +0900

    [luminance-front] Preserve the default type for Tess and TessBuilder.

--- luminance-front/src/tess.rs
+++ luminance-front/src/tess.rs
@@ -5,8 +5,9 @@ pub use luminance::tess::{
   TessViewError,
 };
 
-pub type TessBuilder<'a, V, I, W, S> = luminance::tess::TessBuilder<'a, Backend, V, I, W, S>;
-pub type Tess<V, I, W, S> = luminance::tess::Tess<Backend, V, I, W, S>;
+pub type TessBuilder<'a, V, I = (), W = (), S = Interleaved> =
+  luminance::tess::TessBuilder<'a, Backend, V, I, W, S>;
+pub type Tess<V, I = (), W = (), S = Interleaved> = luminance::tess::Tess<Backend, V, I, W, S>;
 pub type Vertices<V, I, W, S, T> = luminance::tess::Vertices<Backend, V, I, W, S, T>;
 pub type VerticesMut<V, I, W, S, T> = luminance::tess::VerticesMut<Backend, V, I, W, S, T>;
 pub type Indices<V, I, W, S> = luminance::tess::Indices<Backend, V, I, W, S>;
